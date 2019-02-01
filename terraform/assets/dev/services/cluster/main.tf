557ca6c5c12ee4c5d8eecc81824b51a15e65c180
Robert Convery <robconvery@me.com>
2019-02-01 09:53:31 +0000

    Experimenting to see if circle-ci will create a server.

--- terraform/assets/dev/services/cluster/main.tf
+++ terraform/assets/dev/services/cluster/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
     region = "eu-west-1"
 }
+
+resource "aws_instance" "example" {
+    ami = "ami-0e101c2ad1fbe6036"
+    instance_type = "t2.micro"
+
+    tags {
+        Name = "terraform-example"
+    }
+}
+
+
